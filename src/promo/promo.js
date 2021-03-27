55a8ee641216ccab5420a0bc0b4d23030653a0e9
Dvornitskyi <dvornitskyi@gmail.com>
2021-03-28 01:16:53 +0200

    Promo

--- src/promo/promo.js
+++ src/promo/promo.js
@@ -6,13 +6,16 @@ export default function promo() {
     return (
             <div className="promo">
                  <div className="promo__section">
+                 <div className="promo__components">
                     <h2 className="promo__title">At vero eos et accusamus et iusto odio dignissimos ducimus</h2>
-                    <span className="promo__text">Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. </span>
+                    <p className="promo__text">Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. </p>
                     <button className="promo__btn">Try for free </button>
+                   </div> 
                  </div>
-                 {/* <div>
-                    <img src="" />
-                 </div> */}
+                 <div className="promo__section-image">
+                    <img className="promo__iMac" src="../image/promo-iMac.png" />
+                    <img className="promo__image" src="../image/promo-image.png" />
+                  </div>
             </div>
      );
   }
